4371db6cf5cc235c71eb80c41d31c598d38ee668
Artem Zykov <artem.zykov_ext@lemanapro.ru>
2025-04-09 16:16:33 +0700

    Base for koin annotations

--- weather-tracker-compose/shared/build.gradle.kts
+++ weather-tracker-compose/shared/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
 dependencies {
     ksp(libs.room.compiler)
+    ksp(libs.koin.compiler)
 }
 
 room {
@@ -24,31 +25,12 @@ compose.resources {
 
 kotlin {
 
-// Target declarations - add or remove as needed below. These define
-// which platforms this KMP module supports.
-// See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
         namespace = "com.example.weathertracker.shared"
         compileSdk = 35
         minSdk = 32
-
-        withHostTestBuilder {
-        }
-
-        withDeviceTestBuilder {
-            sourceSetTreeName = "test"
-        }.configure {
-            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
     }
 
-// For iOS targets, this is also where you should
-// configure native binary output. For more information, see:
-// https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
-
-// A step-by-step guide on how to include this library in an XCode
-// project can be found here:
-// https://developer.android.com/kotlin/multiplatform/migrate
     val xcfName = "sharedKit"
 
     iosX64 {
@@ -69,11 +51,6 @@ kotlin {
         }
     }
 
-// Source set declarations.
-// Declaring a target automatically creates a source set with the same name. By default, the
-// Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
-// common to share sources between related targets.
-// See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
     sourceSets {
         commonMain {
             dependencies {
@@ -89,6 +66,7 @@ kotlin {
                 implementation(libs.kotlinx.datetime)
 
                 implementation(libs.koin.core)
+                api(libs.koin.annotations)
                 implementation(libs.koin.compose)
                 implementation(libs.koin.compose.viewModel)
 
@@ -103,37 +81,15 @@ kotlin {
             }
         }
 
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
-
         androidMain {
             dependencies {
                 implementation(libs.ktor.engine.android)
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
-            }
-        }
-
-        getByName("androidDeviceTest") {
-            dependencies {
-                implementation(libs.androidx.runner)
-                implementation(libs.androidx.core)
-                implementation(libs.androidx.junit)
             }
         }
 
         iosMain {
             dependencies {
                 implementation(libs.ktor.engine.darwin)
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
             }
         }
     }
