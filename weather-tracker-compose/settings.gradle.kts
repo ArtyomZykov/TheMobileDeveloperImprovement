9e5f1f49e9f7c3cf6950da51f8cacedc117ac6fe
Artem Zykov <artem.zykov_ext@lemanapro.ru>
2025-04-15 00:06:32 +0700

    Setup readme

--- weather-tracker-compose/settings.gradle.kts
+++ weather-tracker-compose/settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
--- weather-tracker-compose/shared/build.gradle.kts
+++ weather-tracker-compose/shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.commonMainImplementation
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -8,87 +10,79 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
-dependencies {
-    ksp(libs.room.compiler)
-}
+private val namespace = "com.example.weathertracker.shared"
 
 room {
     schemaDirectory("$projectDir/schemas")
 }
 
 compose.resources {
-    packageOfResClass = "com.example.weathertracker.shared.resources"
+    packageOfResClass = "$namespace.resources"
     publicResClass = false
     generateResClass = auto
 }
 
 kotlin {
-
     androidLibrary {
-        namespace = "com.example.weathertracker.shared"
+        namespace = namespace
         compileSdk = 35
         minSdk = 32
     }
 
     val xcfName = "sharedKit"
-
     iosX64 {
         binaries.framework {
             baseName = xcfName
         }
     }
-
     iosArm64 {
         binaries.framework {
             baseName = xcfName
         }
     }
-
     iosSimulatorArm64 {
         binaries.framework {
             baseName = xcfName
         }
     }
-
     sourceSets {
-        commonMain {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.components.resources)
-                implementation(libs.precompose)
-
-                implementation(libs.kotlinx.collectionsImmutable)
-                implementation(libs.kotlinx.serialization)
-                implementation(libs.kotlinx.coroutines)
-                implementation(libs.kotlinx.datetime)
-
-                implementation(libs.koin.core)
-                implementation(libs.koin.compose)
-                implementation(libs.koin.compose.viewModel)
-
-                implementation(libs.room.runtime)
-                implementation(libs.sqlite.bundled)
-                implementation(libs.datastore.preferences)
-
-                implementation(libs.ktor)
-                implementation(libs.ktor.logging)
-                implementation(libs.ktor.contentNegotiation)
-                implementation(libs.ktor.kotlinxSerialization)
-            }
-        }
-
         androidMain {
             dependencies {
                 implementation(libs.ktor.engine.android)
             }
         }
-
         iosMain {
             dependencies {
                 implementation(libs.ktor.engine.darwin)
             }
         }
     }
+}
+
+dependencies {
+    ksp(libs.room.compiler)
+
+    commonMainImplementation(compose.runtime)
+    commonMainImplementation(compose.foundation)
+    commonMainImplementation(compose.material3)
+    commonMainImplementation(compose.components.resources)
+    commonMainImplementation(libs.precompose)
+
+    commonMainImplementation(libs.kotlinx.collectionsImmutable)
+    commonMainImplementation(libs.kotlinx.serialization)
+    commonMainImplementation(libs.kotlinx.coroutines)
+    commonMainImplementation(libs.kotlinx.datetime)
+
+    commonMainImplementation(libs.koin.core)
+    commonMainImplementation(libs.koin.compose)
+    commonMainImplementation(libs.koin.compose.viewModel)
+
+    commonMainImplementation(libs.room.runtime)
+    commonMainImplementation(libs.sqlite.bundled)
+    commonMainImplementation(libs.datastore.preferences)
+
+    commonMainImplementation(libs.ktor)
+    commonMainImplementation(libs.ktor.logging)
+    commonMainImplementation(libs.ktor.contentNegotiation)
+    commonMainImplementation(libs.ktor.kotlinxSerialization)
 }
